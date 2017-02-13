ec6068d0a6b2f1617b25ab1c7b4d8eaa35a53a2c
Eric Arnold <eric.ed.arnold@gmail.com>
2017-02-13 01:01:52 -0500

    declare module in all lib files

--- lib/special_sauce/capybara.rb
+++ lib/special_sauce/capybara.rb
@@ -1,20 +1,22 @@
-class SpecialSauce::Capybara < SpecialSauce::Driver
+module SpecialSauce
+  class Capybara < SpecialSauce::Driver
 
-  def self.current_session(options = {})
-    puts "Sauce Labs auth ENV variables not set." && return unless auth?
+    def self.current_session(options = {})
+      puts "Sauce Labs auth ENV variables not set." && return unless auth?
 
-    ::Capybara.register_driver :special_sauce do |app|
-      ::Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: sauce_endpoint,
-        desired_capabilities: desired_capabilities(options)
-      )
+      ::Capybara.register_driver :special_sauce do |app|
+        ::Capybara::Selenium::Driver.new(
+          app,
+          browser: :remote,
+          url: sauce_endpoint,
+          desired_capabilities: desired_capabilities(options)
+        )
+      end
+
+      ::Capybara.current_driver = :special_sauce
+
+      ::Capybara.current_session
     end
 
-    ::Capybara.current_driver = :special_sauce
-
-    ::Capybara.current_session
   end
-
 end
